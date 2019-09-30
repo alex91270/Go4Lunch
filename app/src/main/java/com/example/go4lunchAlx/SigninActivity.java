d684c3ace8aee771c87965610c2c38efb7dead1e
alex <alex91270@gmail.com>
2019-09-30 20:51:55 +0200

    initial commit

---
+++ app/src/main/java/com/example/go4lunchAlx/SigninActivity.java
@@ -0,0 +1,13 @@
+package com.example.go4lunchAlx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
